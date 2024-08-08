98a652352d72f2417d794666d84cd2dc686171ac
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-08-08 13:48:56 +0800

    Implement form-handler for submission of snip

---
+++ public/form-handler.js
@@ -0,0 +1,73 @@
+const resultBaseLink = 'http://localhost:3000';
+const backendApi = 'http://localhost:8080/api/v1/snip';
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const jsonData = {
+    'content': formData.get('content'),
+    'expiryDate': formData.get('expiry-date')
+  };
+
+  result.innerHTML = `
+  <hgroup>
+    <h1>SUBMITTING...<h1>
+    <p>Please wait a moment...</p>
+  </hgroup>
+  `;
+
+  setFormInteractivity(false)
+  submitForm(jsonData)
+});
+
+function setFormInteractivity(isInteractive) {
+  form.querySelectorAll('input, textarea, button, select')
+    .forEach(element => {
+      element.disabled = !isInteractive;
+    })
+}
+
+async function submitForm(jsonData) {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  fetch(backendApi, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(jsonData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      return response.json();
+    })
+    .then(data => {
+      updateResult(data.retrievalId);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+}
+
+function updateResult(linkResult) {
+
+  const link = `${resultBaseLink}/${linkResult}`;
+
+  result.innerHTML = `
+  <hgroup>
+    <h1>DONE!</h1>
+    <p>Copy and share this link to view the snippet!</p>
+  </hgroup>
+  <a href=${link} target="_blank" rel="noopener noreferrer">${link}</a>
+  `;
+
+  setFormInteractivity(true)
+}
