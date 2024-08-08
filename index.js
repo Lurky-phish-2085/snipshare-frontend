3e5b0069eb97e9292b872603c5942d2599ae54eb
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-08-08 14:29:35 +0800

    Implement html page for viewing snippets

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = 3000;
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'snippet-view.html'))
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
---
+++ public/fetch-snip.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const data = window
+
+  const content = document.getElementById('content');
+  content.innerHTML = 'TEST: ' + data;
+})
