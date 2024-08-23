8050d6cd1dd0767ead129139e5f0058d179718b1
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-08-23 13:28:22 +0800

    Add disposable snip warning that deletes snips when opened

--- public/js/fetch-snip.js
+++ public/js/fetch-snip.js
@@ -1,7 +1,8 @@
 const backendApi = 'http://localhost:8080/api/v1/snip';
+let retrievalId = null;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const retrievalId = window.userId;
+  retrievalId = window.userId;
   fetchSnippet(retrievalId)
 });
 
@@ -17,15 +18,87 @@ async function fetchSnippet(retrievalId) {
       return response.json();
     })
     .then(data => {
-      updateContent(data.content)
+      updateContent(data);
     })
     .catch(error => {
       updateContent(error);
     })
 }
 
-function updateContent(contentDisplay) {
-  const content = document.getElementById('content');
+const disposableSnipWarningHTML = `
+      <article id="delete-warning">
+        <h2>Warning! Disposable Snip!</h2>
+        <p>
+          This snip is <strong>disposable</strong> and will be <strong>disposed</strong> after you read
+          it. 
+        </p>
+        <p>Do you wish to continue?</p>
+        <button id="open-snip-btn" type="submit">Open Snip</button>
+      </article>
+`;
 
-  content.innerText = contentDisplay;
+const snipDisplayHTML = `
+      <div id="snip-display">
+        <h2>Content</h2>
+        <article id="content">
+          <h4>LOADING...</h4>
+        </article>
+      </div>
+`;
+
+const notFoundHTML = `
+      <article>
+        <h2>NOT FOUND</h2>
+        <p>
+          Snip must have been expired, disposed, or deleted by the owner.
+        </p>
+        <button id="create-snip-btn" type="submit">Create Snip</button>
+      </article>
+`;
+
+function updateContent(snipData) {
+  const main = document.getElementById('main');
+  let deleteWarning = null;
+  let openSnipBtn = null;
+
+  if (snipData instanceof Error) {
+    main.innerHTML = notFoundHTML;
+    const createSnipBtn = document.getElementById('create-snip-btn');
+    createSnipBtn.addEventListener('click', (event) => {
+      window.location.href = 'http://localhost:3000'
+    })
+    return;
+  }
+
+  if (snipData.isDisposable) {
+    main.innerHTML = disposableSnipWarningHTML;
+    deleteWarning = document.getElementById('delete-warning');
+    openSnipBtn = document.getElementById('open-snip-btn');
+
+    openSnipBtn.addEventListener('click', (event) => {
+      openSnipBtn.disabled = true;
+
+      fetch(`${backendApi}/${retrievalId}`, 
+        {
+          method: 'DELETE'
+        }
+      ).then(response => {
+          if (!response.status === 204) {
+            throw new Error('Network response was not ok');
+          }
+
+          deleteWarning.remove();
+          main.innerHTML = snipDisplayHTML;
+          const content = document.getElementById('content');
+          content.innerText = snipData.content;
+      }).catch(error => {
+          console.error(error);
+      })
+
+    })
+  } else {
+      main.innerHTML = snipDisplayHTML;
+      const content = document.getElementById('content');
+      content.innerText = snipData.content;
+  }
 }
