2269d0ceed99e27f5b53ec801f595a2807ae87e7
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-18 13:59:20 +0800

    Add middleware to setDefaultHeader and show 404 page
    Preventing the browser from making a cache of a page ensures that we get
    the latest data from the backend.

--- src/app.js
+++ src/app.js
@@ -6,8 +6,35 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
+
+app.use(setDefaultHeaders());
+
 app.use(require("./routes/webpagesRoutes"));
+
+app.use(notFoundPage());
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
 });
+
+function setDefaultHeaders() {
+  return (req, res, next) => {
+    preventCaching(res);
+    next();
+  };
+}
+
+function preventCaching(res) {
+  res.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+}
+
+function notFoundPage() {
+  return (req, res, next) => {
+    res.status(404).render("notFound");
+  };
+}
