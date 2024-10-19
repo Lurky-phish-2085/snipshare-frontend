4eff789bff20855a9e5423edaef3b461f4ced36a
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-10-19 13:41:13 +0800

    Change submitSnip response to redirect instead
    Since we can parse url encoded data now, the request body we receive
    from the form tag of an html which calls this endpoint will now be
    parsed and the redirection will work as expected.

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -1,11 +1,12 @@
 const Snip = require("../models/snipModel");
 
 const submitSnip = async (req, res) => {
-  const { content, title, isDisposable, expiryDate } = req.body;
-  const snip = new Snip(content, title, isDisposable, expiryDate);
+  const { content, title, disposable, expiryDate } = req.body;
+
+  const snip = new Snip(content, title, disposable === "true", expiryDate);
   const id = await snip.save();
 
-  res.status(200).send({ redirectUrl: `/${id}` });
+  res.redirect(`/${id}`);
 };
 
 module.exports = {
