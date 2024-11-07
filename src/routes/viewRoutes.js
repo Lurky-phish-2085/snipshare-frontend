141a17ed14e1784ed7ae6cd2e5ecf54e77a7ecc4
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-11-07 11:31:36 +0800

    Apply redirectWhenAuthenticated middleware for login and register
    Prevents an authenticated user from visting the login
    and registration page.

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const requireAuth = require("../middlewares/requireAuth");
+const redirectWhenAuthenticated = require("../middlewares/redirectWhenAuthenticated");
 const handler = require("../controllers/viewController");
 
 const router = express.Router();
 
 router.get("/", handler.index);
-router.get("/login", handler.login);
-router.get("/register", handler.register);
+router.get("/login", redirectWhenAuthenticated(), handler.login);
+router.get("/register", redirectWhenAuthenticated(), handler.register);
 router.get("/profile", requireAuth(), handler.profile);
 router.get("/:id", handler.snippet);
 
