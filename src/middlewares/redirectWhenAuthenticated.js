10f94946ca4033bc9e74a6f57efd1c67d855399a
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2024-11-07 19:24:32 +0800

    Fix faulty early returns

--- src/middlewares/redirectWhenAuthenticated.js
+++ src/middlewares/redirectWhenAuthenticated.js
@@ -1,6 +1,9 @@
 function redirectWhenAuthenticated(route = "/") {
   return (req, res, next) => {
-    if (!req.authUser) next();
+    if (!req.authUser) {
+      next();
+      return;
+    }
 
     res.redirect(route);
   };
